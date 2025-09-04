ed25abd2e8ef4877960a1fb2fe465709ba848edb
Muhammad Talha <you@example.com>
2025-09-04 20:19:45 +0500

    Add symbol data type example and comparison in datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -1,6 +1,8 @@
 let name = "talha"
 let age = 21;
 let isLoggedIn = true;
+let id = Symbol("123");
+console.log(id === age);
 console.log(typeof name);
 console.log(typeof null);
 console.log(typeof undefined);
