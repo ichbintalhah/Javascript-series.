a22100c1b42f0b46a08989c184c5403b92f429aa
Muhammad Talha <you@example.com>
2025-09-04 21:00:04 +0500

    Add additional string manipulation examples in templateliterals.js

--- templateliterals.js
+++ templateliterals.js
@@ -6,3 +6,7 @@ let profession = "Web Developer";
 let myDetails = `Hello my name is ${name}. I am ${age} years old. I live in ${city} and I am a ${profession}.`;
 
 console.log(myDetails);
+console.log(name.toUpperCase());
+console.log(name.charAt(4));
+console.log(name.indexOf("a"));
+
