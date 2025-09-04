b65684c640e035751f52d7bacf73417af5a737b7
Muhammad Talha <you@example.com>
2025-09-04 20:45:19 +0500

    Add template literals example with personal details in templateliterals.js

---
+++ templateliterals.js
@@ -0,0 +1,8 @@
+let name = "Talha";
+let age = 21;
+let city = "Lahore";
+let profession = "Web Developer";
+
+let myDetails = `Hello my name is ${name}. I am ${age} years old. I live in ${city} and I am a ${profession}.`;
+
+console.log(myDetails);
