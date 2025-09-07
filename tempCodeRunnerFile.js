261c76066db99c037fa0bb25befe0545afaf7fd0
Muhammad Talha <you@example.com>
2025-09-08 00:23:51 +0500

    Remove array.js and update tempCodeRunnerFile.js to log array

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,3 @@
-let num1 = 5;
-// let num2 = num1; 
-// num2 = 10;
-// console.log(num1); // here num1 will be 5 because it is stored in stack memory and when we assigned num1 to num2 it created a copy of num1 in num2
-// co
+console.log(array);
+// console.log(myArray);
+
